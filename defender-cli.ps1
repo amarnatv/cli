7a01deacf881736303917150943e046d58ff249c
amarnatv <48459179+amarnatv@users.noreply.github.com>
2025-04-15 13:08:31 -0700

    Update defender-cli.ps1

--- defender-cli.ps1
+++ defender-cli.ps1
@@ -91,6 +91,7 @@ if ($OS_TYPE -eq "windows") {
 #} else {
 #    $INSTALL_DIRS="/usr/local/bin /usr/bin /opt/bin $home"
 #}
+exit 0
 
 #foreach ($InstallDir in $env:Path -split ";") {
 foreach ($InstallDir in $INSTALL_DIRS -split " ") {
